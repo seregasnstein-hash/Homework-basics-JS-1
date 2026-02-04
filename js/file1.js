caaa76c31b08898e766a61b8e800c3b5a558e7f4
Сенченко Сергей <serega-stein@mail.ru>
2026-02-04 14:31:44 +0500

    Добавлена первая задача, какое число больше

---
+++ js/file1.js
@@ -0,0 +1,6 @@
+const num1 = parseInt(prompt('Введите первое число:'));
+const num2 = parseInt(prompt('Введите второе число:'));
+
+whoIsBigger = num1 > num2 ? num1 : num2;
+
+alert(`${whoIsBigger} - это число больше`);
