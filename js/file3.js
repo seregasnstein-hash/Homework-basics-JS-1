4a812320db9e26a60a45c0a836bbc38eda0715a8
Сенченко Сергей <serega-stein@mail.ru>
2026-02-04 15:29:07 +0500

    задача №3 времена года, добавлен спагетти код с условиями

---
+++ js/file3.js
@@ -0,0 +1,41 @@
+const monthNumber = parseInt(prompt('Введите номер месяца:'));
+
+if(monthNumber === 1 ){
+    alert(`${monthNumber} время года вашего месяца - зима`);
+}
+else if(monthNumber === 2){
+    alert(`${monthNumber} время года вашего месяца - зима`);
+}
+else if(monthNumber === 3){
+    alert(`${monthNumber} время года вашего месяца - весна`);
+}
+else if(monthNumber === 4){
+    alert(`${monthNumber} время года вашего месяца - весна`);
+}
+else if(monthNumber === 5){
+    alert(`${monthNumber} время года вашего месяца - весна`);
+}
+else if(monthNumber === 6){
+    alert(`${monthNumber} время года вашего месяца - лето`);
+}
+else if(monthNumber === 7){
+    alert(`${monthNumber} время года вашего месяца - лето`);
+}
+else if(monthNumber === 8){
+    alert(`${monthNumber} время года вашего месяца - лето`);
+}
+else if(monthNumber === 9){
+    alert(`${monthNumber} время года вашего месяца - осень`);
+}
+else if(monthNumber === 10){
+    alert(`${monthNumber} время года вашего месяца - осень`);
+}
+else if(monthNumber === 11){
+    alert(`${monthNumber} время года вашего месяца - осень`);
+}
+else if(monthNumber === 12){
+    alert(`${monthNumber} время года вашего месяца - зима`);
+}
+else{
+    alert(`${monthNumber} такого номера месяца не существует!`)
+}
