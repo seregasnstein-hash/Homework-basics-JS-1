d1f4117937950b449770334dac1ef529829cc32f
Сенченко Сергей <serega-stein@mail.ru>
2026-02-06 21:20:05 +0500

    оптимизирован спагетти код (задача - время года)

--- js/file3.js
+++ js/file3.js
@@ -1,40 +1,16 @@
 const monthNumber = parseInt(prompt('Введите номер месяца:'));
 
-if(monthNumber === 1 ){
+if(monthNumber === 1 || monthNumber === 2 || monthNumber === 12 ){
     alert(`${monthNumber} время года вашего месяца - зима`);
 }
-else if(monthNumber === 2){
-    alert(`${monthNumber} время года вашего месяца - зима`);
-}
-else if(monthNumber === 3){
+else if(monthNumber >= 3 && monthNumber <= 5){
     alert(`${monthNumber} время года вашего месяца - весна`);
 }
-else if(monthNumber === 4){
-    alert(`${monthNumber} время года вашего месяца - весна`);
-}
-else if(monthNumber === 5){
-    alert(`${monthNumber} время года вашего месяца - весна`);
-}
-else if(monthNumber === 6){
+else if(monthNumber >= 6 && monthNumber <= 8){
     alert(`${monthNumber} время года вашего месяца - лето`);
 }
-else if(monthNumber === 7){
-    alert(`${monthNumber} время года вашего месяца - лето`);
-}
-else if(monthNumber === 8){
-    alert(`${monthNumber} время года вашего месяца - лето`);
-}
-else if(monthNumber === 9){
+else if(monthNumber >= 9 && monthNumber <= 11){
     alert(`${monthNumber} время года вашего месяца - осень`);
-}
-else if(monthNumber === 10){
-    alert(`${monthNumber} время года вашего месяца - осень`);
-}
-else if(monthNumber === 11){
-    alert(`${monthNumber} время года вашего месяца - осень`);
-}
-else if(monthNumber === 12){
-    alert(`${monthNumber} время года вашего месяца - зима`);
 }
 else{
     alert(`${monthNumber} такого номера месяца не существует!`)
